a233ef231baa767ccfe6b863d6c7db92c617c947
dobrin.marinov <dobrin.marinov@musala.com>
2020-03-06 09:37:10 +0200

    Refactored visibility animation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,22 +7,14 @@ import { switchMap } from 'rxjs/operators';
 import { Comment } from '../shared/comment';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ValidationService } from '../services/validation.service';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { visibility } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)', opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)', opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility()
   ]
 })
 export class DishdetailComponent implements OnInit {
